17208a8479e57bf62fcc89a5f512480e780d3293
mahmoodhamza1261 <mahmoodhamza1261@gmail.com>
2023-08-08 13:14:37 +0500

    3

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import Head from 'next/head'
 import { Footer, Navbar } from '../components'
 
-const Layout = ({children}) => {
-  return (
-    <div className='layout'>
-        <Head>
-            <title>Dine Market</title>
-        </Head>
-        <header>
-            <Navbar />
-        </header>
-        <main className='main-container'>
-            {children}
-        </main>
-        <footer>
-            <Footer />
-        </footer>
-    </div>
-  )
+const Layout = ({ children }) => {
+    return (
+        <div className='layout'>
+            <Head>
+                <title>E-commerce Shopping Store</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className='main-container'>
+                {children}
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
+    )
 }
 
 export default Layout
